58fd991094daf61b53df6920673cd25771cdf93b
Yuri²² <viagostinho2007@gmail.com>
2024-11-29 12:20:14 -0300

    último

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Top Gun: Maverick',
+    'Qual é o enredo principal de Top Gun: Maverick?',
+    'Pete "Maverick" Mitchell retorna como instrutor na Top Gun para treinar uma nova geração de pilotos em uma missão perigosa, enquanto enfrenta fantasmas do passado, incluindo sua relação com Rooster, filho de Goose.'
+)
+
+criaCartao(
+    'Top Gun: Maverick',
+    'Quais atores principais estão em Top Gun: Maverick?',
+    'Tom Cruise como Maverick, Miles Teller como Rooster, Jennifer Connelly como Penny, e Glen Powell como Hangman.'
+)
+
+criaCartao(
+    'Top Gun: Maverick',
+    'O filme usou efeitos especiais ou aviões reais nas cenas de voo?',
+    'As cenas foram filmadas com aviões reais! Os atores passaram por treinamento intenso para aguentar as forças gravitacionais e trazer mais autenticidade ao filme.'
+)
+
+criaCartao(
+    'Top Gun: Maverick',
+    'Preciso assistir ao primeiro Top Gun para entender Maverick?',
+    'Não é obrigatório, mas assistir ao primeiro ajuda a entender as referências e conexões emocionais, especialmente sobre Maverick e Goose.'
+)
+
+criaCartao(
+    'Top Gun: Maverick',
+    'Qual é o tema central de Top Gun: Maverick?',
+    'O filme explora legado, superação, sacrifício e a passagem de conhecimento para uma nova geração, além de equilibrar ação e emoção.'
+)
